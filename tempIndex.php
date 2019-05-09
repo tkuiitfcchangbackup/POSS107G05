06808284242bdc2da36c19483958325d39a13494
NicholasWu <james12313@gmail.com>
2019-05-09 08:17:01 +0800

    lol

---
+++ tempIndex.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+
+</head>
+<body>
+  <form action="react.php" method="post">
+  你的問題 :
+  <input type ="text" answer="answer" >
+  <input type ="reset" value="清除, 重寫">
+  <input type ="submit" value="送出">
+  </form>
+</body>
+</html>
+<?php
+
+?>
+<form action="react.php" method="post">
+你的問題 :
+<input type ="text" answer="answer" >
+<input type ="reset" value="清除, 重寫">
+<input type ="submit" value="送出">
+</form>
