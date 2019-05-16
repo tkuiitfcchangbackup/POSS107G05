feef9e1ea2101603cba83c48f25b0f18db4ed616
ShinjiKun <james12313@gmail.com>
2019-05-16 12:28:44 +0800

    update group introduction.

--- tempIndex.php
+++ tempIndex.php
@@ -3,6 +3,10 @@
 
 </head>
 <body>
+<h3>I am Wilson Chen, 19 years old studied in Tamkung University, major in IIT department.</h3>
+<h3>My name is Jeff.</h3>
+<h3>My new instagram post about to promote in May 17, please follow “gerrard_mas”on instagram to see more</h3>
+<h3>My name is Yu-Cheng Zhou</h3>
   <form action="react.php" method="post">
   你的問題 :
   <input type ="text" answer="answer" >
@@ -14,9 +18,3 @@
 <?php
 
 ?>
-<form action="react.php" method="post">
-你的問題 :
-<input type ="text" answer="answer" >
-<input type ="reset" value="清除, 重寫">
-<input type ="submit" value="送出">
-</form>
