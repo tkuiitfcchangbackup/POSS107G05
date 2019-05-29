5e9155a12ae3844ad3ba0c82cf4c7b70c4a78757
nicholas <james12313@gmail.com>
2019-05-29 08:15:39 +0000

    weekly progress(?)

--- action_page.php
+++ action_page.php
@@ -1,7 +1,15 @@
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+</head>
 <?php
 $search=$_POST["searchQuest"];
+echo "The word you are looking👀 for is :"
 ?>
-your word is :
+
 <?php
 echo $search;
 ?>
+<br>
+<br>
+<br>
+<a href="index.html">Back to front page</a>
