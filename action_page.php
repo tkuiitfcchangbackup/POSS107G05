475851181e75e0deb94cecfa45fa28cf3dafd7c0
nico12313 <james12313@gmail.com>
2019-06-12 14:35:52 +0000

    update mysqli part (thank you teacher~~)

--- action_page.php
+++ action_page.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
@@ -8,13 +10,6 @@
 $search=$_POST["searchQuest"];
 $u_name=$_POST["searchName"];
 $s_result="zErO";
-?>
-<!--
-
-$con = mysql_connect("localhost","nicholas","12313");
-if(!$con){
-  die('Could not connect: '.mysql_error());
-}
 
 $servername = "localhost";
 $username = "nicholas";
@@ -29,53 +24,66 @@ if (!$conn) {
 }
 
 $sql = "INSERT INTO rec (user_id, word_req, word_trans)
-VALUES ('John', 'Doe', 'marry')";
+VALUES ('$u_name', '$search', '$s_result')";
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+    echo "<br><br>";
+    echo "The word you are looking for 👀 is : ";
+    echo "<h2>".$search."</h2><br>";
+    $Brah="https://www.urbandictionary.com/define.php?term=".$search;
+    $brah=str_replace(" ","+",$Brah);
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
 mysqli_close($conn);
 
-
-mysql_select_db('the_db', $con);
-
-mysql_query("INSERT INTO `rec` (`user_id`,`word_req`,`word_trans`)
-VALUES ('{$u_name}', '{$search}', '{$s_result}')");
-
-mysql_close($con);
--->
-<?php
-echo "<br><br>";
-echo "The word you are looking for 👀 is : ";
-echo "<h2>".$search."</h2><br>";
-$Brah="https://www.urbandictionary.com/define.php?term=".$search;
-$brah=str_replace(" ","+",$Brah);
 ?>
 
 <a href="index.html"><h3>>>BACK TO FRONT PAGE<<</h3></a>
 <br>
-</div>
+<br>
 
 <?php
 $url=$brah;
-$ch=curl_init();
-$timeout=5;
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-$lines_string=curl_exec($ch);
-curl_close($ch);
-echo "$lines_string";
+//$ch=curl_init();
+//$timeout=5;
+//curl_setopt($ch, CURLOPT_URL, $url);
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+//$lines_string=curl_exec($ch);
+//curl_close($ch);
+//echo "$lines_string";
 
+$fp = file_get_contents($url);
+preg_match_all("/<div class=\"meaning\">.*?<\/div>/", $fp, $match);
+
+if (count($match[0])>0) {
+    逐筆印出
+    foreach ($match[0] as $key => $value) {
+        echo $value;
+    }
+}else{
+	echo "無資料";
+}
+
+//$html = "<b>bold text</b><a href=howdy.html>click me</a>";
+
+//preg_match_all("/(<([\w]+)[^>]*>)(.*?)(<\/\\2>)/", $html, $matches, PREG_SET_ORDER);
+
+//foreach ($matches as $val) {
+    //echo "matched: " . $val[0] . "\n";
+    //echo "part 1: " . $val[1] . "\n";
+    //echo "part 2: " . $val[2] . "\n";
+    //echo "part 3: " . $val[3] . "\n";
+    //echo "part 4: " . $val[4] . "\n\n";
+//}
 ?>
 
-<div style='text-align:center'>
 <br>
 <br>
 <br>
 <a href="index.html">Brah~</a>
 </div>
 </body>
+</html>
