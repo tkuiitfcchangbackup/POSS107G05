739ca968f6faf3cd06f35c645df33e56e99b9129
nico12313 <james12313@gmail.com>
2019-06-12 20:16:18 +0000

    maybe the final update

--- action_page.php
+++ action_page.php
@@ -4,17 +4,36 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
 </head>
-<body>
+<body style="background-color:black;">
+<center>
 <div style='text-align:center'>
+<div style="width:350px;background-color:#D4E6F8;">
 <?php
 $search=$_POST["searchQuest"];
 $u_name=$_POST["searchName"];
-$s_result="zErO";
 
 $servername = "localhost";
 $username = "nicholas";
 $password = "12313";
 $dbname = "the_db";
+
+$Brah="https://www.urbandictionary.com/define.php?term=".$search;
+$brah=str_replace(" ","+",$Brah);
+
+$url = $brah;
+$contents = file_get_contents($url);
+//如果出現中文亂碼使用下面程式碼
+//$getcontent = iconv("gb2312", "utf-8",$contents);
+//echo $contents;
+$from="<div class=\"meaning\">";
+$end="</div>";
+function cut($file,$from,$end){
+$message=explode($from,$file);
+$message=explode($end,$message[1]);
+return $message[0];
+}
+$q=cut($contents, $from, $end);
+$p=strip_tags($q);
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -24,66 +43,59 @@ if (!$conn) {
 }
 
 $sql = "INSERT INTO rec (user_id, word_req, word_trans)
-VALUES ('$u_name', '$search', '$s_result')";
+VALUES ('$u_name', '$search', '$p')";
 
 if (mysqli_query($conn, $sql)) {
     echo "<br><br>";
     echo "The word you are looking for 👀 is : ";
     echo "<h2>".$search."</h2><br>";
-    $Brah="https://www.urbandictionary.com/define.php?term=".$search;
-    $brah=str_replace(" ","+",$Brah);
 } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    echo "Error: ".$sql."<br>".mysqli_error($conn);
 }
 
 mysqli_close($conn);
 
 ?>
-
-<a href="index.html"><h3>>>BACK TO FRONT PAGE<<</h3></a>
-<br>
-<br>
-
-<?php
-$url=$brah;
-//$ch=curl_init();
-//$timeout=5;
-//curl_setopt($ch, CURLOPT_URL, $url);
-//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-//curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-//$lines_string=curl_exec($ch);
-//curl_close($ch);
-//echo "$lines_string";
-
-$fp = file_get_contents($url);
-preg_match_all("/<div class=\"meaning\">.*?<\/div>/", $fp, $match);
-
-if (count($match[0])>0) {
-    逐筆印出
-    foreach ($match[0] as $key => $value) {
-        echo $value;
-    }
-}else{
-	echo "無資料";
-}
-
-//$html = "<b>bold text</b><a href=howdy.html>click me</a>";
-
-//preg_match_all("/(<([\w]+)[^>]*>)(.*?)(<\/\\2>)/", $html, $matches, PREG_SET_ORDER);
-
-//foreach ($matches as $val) {
-    //echo "matched: " . $val[0] . "\n";
-    //echo "part 1: " . $val[1] . "\n";
-    //echo "part 2: " . $val[2] . "\n";
-    //echo "part 3: " . $val[3] . "\n";
-    //echo "part 4: " . $val[4] . "\n\n";
-//}
-?>
-
-<br>
-<br>
-<br>
-<a href="index.html">Brah~</a>
 </div>
+</center>
+<a href="index.php"><h3>>>BACK TO FRONT PAGE<<</h3></a>
+<br>
+<br>
+
+<center>
+<div style="width:350px;background-color:#D4E6F8;">
+<table border="1">
+<tr>
+<td style="width:350px;">
+<table style="border:3px #FFFFFF solid;padding:5px;" rules="all" cellpadding='5';>
+<?php
+
+echo $p;
+
+$from="<div class=\"example\">";
+$end="</div>";
+$q=cut($contents, $from, $end);
+$p=strip_tags($q);
+echo "<br><br>".$p;
+
+$from="<div class=\"contributor\">";
+$end="</div>";
+$q=cut($contents, $from, $end);
+$p=strip_tags($q);
+echo "<br><br>".$p;
+?>
+</table>
+</td>
+</tr>
+</table>
+</div>
+</center>
+
+<br>
+<br>
+<br>
+<a href="index.php"><h3>>>BACK TO FRONT PAGE<<</h3></a>
+</div>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
+<!DOCTYPE html>
 <html>
-
 <head>
 <title>Mushroom Dictionary!</title>
 <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
@@ -7,11 +7,9 @@
 <link rel="icon" href="favicon.ico" type="image/x-icon" />
 <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 </head>
-
 <body style="background-color:black;">
-<script>document.location.href="index.php"</script>
 <center>
-<div style="width:350px;height:305px;background-color:#D4E6F8;">
+<div style="width:350px;background-color:#D4E6F8;">
 <table style="text-align:center;"border="1">
 <tr>
 <td style="width:350px;height:300px;">
@@ -33,7 +31,7 @@
 <form action="/POSS107G05/action_page.php" method="post">
   <font color="black" size="3">Your name:<br></font>
   <input type="text" name="searchName">
-  <br>	
+  <br>
   <font color="black" size="3">Searching word:<br></font>
   <input type="text" name="searchQuest">
   <br><br>
@@ -44,12 +42,47 @@
 </table>
 </div>
 <br><br><br>
+
+<div style="width:350px;background-color:#D4E6F8;">
 <table style="text-align:center;"border="1">
 <tr>
 <td style="width:350px;">
 <table style="border:3px #FFFFFF solid;padding:5px;" rules="all" cellpadding='5';>
-<font color="white"><h2>Search record : </h2></font>
-
+<font color="black"><h2>Search record : </h2></font>
+<center id="commentArea">
+<table style="border:3px #FFFFFF solid;padding:5px;" rules="all" cellpadding='5';>
+<p>
+<?php
+$link=mysqli_connect("localhost","nicholas","12313") or die ("無法開啟Mysql資料庫連結"); //建立mysql資料庫連結
+mysqli_select_db($link, "the_db"); //選擇資料庫abc
+$sql = "SELECT update_time,user_id,word_req,word_trans FROM rec"; //在test資料表中選擇所有欄位
+mysqli_query($link,'SET CHARACTER SET utf8');	// 送出Big5編碼的MySQL指令
+mysqli_query($link,"SET collation_connection 'utf8'");
+$result = mysqli_query($link,$sql); // 執行SQL查詢
+$total_fields=mysqli_num_fields($result); // 取得欄位數
+$total_records=mysqli_num_rows($result);  // 取得記錄數
+?>
+</p>
+<table  border="1" align="center">
+<tr>
+<td>_Time_</td>
+<td>_Name_</td>
+<td>_Word_</td>
+<td>_Result_</td>
+</tr>
+<?php
+for ($i=0;$i<$total_records;$i++) {$row = mysqli_fetch_assoc($result); //將陣列以欄位名索引
+?>
+<tr>
+<td><?php echo $row[update_time];?></td>
+<td><?php echo $row[user_id];?></td>
+<td><?php echo $row[word_req];?></td>
+<td><?php echo $row[word_trans];?></td>
+</tr>
+<?php    }   ?>
+</table>
+</table>
+</center>
 </table>
 </td>
 <tr>
